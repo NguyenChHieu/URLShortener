61791c6e4342b74a8201cf982d706751a899ffbf
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-07 21:58:29 +0700

    init

---
+++ Linklytics/src/main/java/com/url/shortener/LinklyticsApplication.java
@@ -0,0 +1,13 @@
+package com.url.shortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinklyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinklyticsApplication.class, args);
+    }
+
+}
