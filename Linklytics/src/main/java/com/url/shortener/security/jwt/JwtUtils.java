ac5c3f72bda954776405f06add4a5fc7a59dd8b6
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-09 13:50:29 +0700

    jwt

---
+++ Linklytics/src/main/java/com/url/shortener/security/jwt/JwtUtils.java
@@ -0,0 +1,71 @@
+package com.url.shortener.security.jwt;
+
+import com.url.shortener.service.UserDetailsImpl;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+public class JwtUtils {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+
+    // Auth Header -> Bearer <Token> -> Token
+    public String getJwtFromHeader(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    /*
+    Generates a JWT for a given user.
+    It includes the username and roles as claims,
+    sets the issued and expiration dates, and
+    signs the token with the secret key.
+     */
+    public String generateToken(UserDetailsImpl userDetails){
+        String username = userDetails.getUsername();
+        String roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+        return Jwts.builder()
+                .subject(username)
+                .claim("role", roles)
+                .issuedAt(new Date())
+                .expiration(new Date((new Date().getTime() + jwtExpirationMs)))
+                .signWith(key())
+                .compact();
+    }
+
+    // Parses the JWT to extract the username (subject) from the token.
+    public String getUserNameFromJwtToken(String token){
+        return Jwts.parser()
+                .verifyWith((SecretKey) key())
+                .build().parseSignedClaims(token)
+                .getPayload().getSubject();
+    }
+
+    // Decodes the base64-encoded secret key and returns it as a Key object for signing and verifying the JWT.
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public boolean validateToken(String authToken){
+            Jwts.parser().verifyWith((SecretKey) key())
+                    .build().parseSignedClaims(authToken);
+            return true;
+    }
+}
