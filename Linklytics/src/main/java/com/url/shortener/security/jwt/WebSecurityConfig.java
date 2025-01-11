8df9f768fc8b1896b31625791afd16b6f14dd86d
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-11 11:07:10 +0700

    web config

--- Linklytics/src/main/java/com/url/shortener/security/jwt/WebSecurityConfig.java
+++ Linklytics/src/main/java/com/url/shortener/security/jwt/WebSecurityConfig.java
@@ -40,6 +40,7 @@ public class WebSecurityConfig {
         return provider;
     }
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
