72717952b0addefa4ab3e03c5968691ba6910bdc
NguyenChHieu <nguyenchihieu178205@gmail.com>
2025-01-14 22:25:05 +0700

    create url func

--- shortener-frontend/src/components/Dashboard/DashboardLayout.jsx
+++ shortener-frontend/src/components/Dashboard/DashboardLayout.jsx
@@ -3,10 +3,13 @@ import {dummyData} from '../dummyData/data'
 import { useStoreContext } from "../../context/ContextApi"
 import { useFetchTotalClicks } from "../../hooks/useQuery"
 import { data } from "react-router-dom"
+import { useState } from "react"
+import ShortenPopUp from "./ShortenPopUp"
 
 const DashboardLayout = () => {
-
+  const refetch = false
   const {token} = useStoreContext()
+  const [shortenPopUp, setShortenPopUp] = useState(false)
 
   // console.log(useFetchTotalClicks(token, onError));
   const {isLoading: loader, data: totalClicks} = useFetchTotalClicks(token, onError);
@@ -39,7 +42,8 @@ const DashboardLayout = () => {
 
             <div className="py-5 sm:text-end text-center">
               <button
-                className="bg-custom-gradient px-4 py-2 rounded-md text-white">
+                className="bg-custom-gradient px-4 py-2 rounded-md text-white"
+                onClick={() => setShortenPopUp(true)}>
                 Create a new Short URL
               </button>
             </div>
@@ -47,6 +51,12 @@ const DashboardLayout = () => {
           </div>
         </div>)
         }
+
+        <ShortenPopUp
+          refetch={refetch}
+          open={shortenPopUp}
+          setOpen={setShortenPopUp}
+        />
     </div>
   )
 }
